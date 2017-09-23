a8df8fd451a59439a47825e6b6c63baf3f104007
chell-uoxou <chell_uoxou@yahoo.co.jp>
2017-09-23 10:01:55 +0900

    First add.

---
+++ src/chell_uoxou/main.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chell_uoxou
+ * Date: 2017/05/24
+ * Time: 午後 4:03
+ */
+namespace HyrulePVPSystem;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+}
